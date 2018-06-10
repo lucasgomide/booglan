96c983ea50e87985184980eab797d64d536e9ace
lucasgomide <lucaslg200@gmail.com>
2018-06-09 23:49:34 -0700

    Adding simplecov and codecov

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem 'pry'
+gem 'codecov', require: false, group: :test
+gem 'simplecov', require: false, group: :test
--- test/unit/run.rb
+++ test/unit/run.rb
@@ -1,4 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CODECOV_TOKEN']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 Dir[
   File.join(__dir__, '*_test.rb'), File.join(__dir__, '*', '*_test.rb')
 ].each { |file| require file }
-
